d09a6eb037ae2dcca4661f31b15ef68e1781802f
Brian Dixon <dbrian463@gmail.com>
2022-01-10 23:23:53 -0500

    made first piano keys

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ function welcomer(){
 }
 
 welcomer();
+
+//step 1: connect keyboard to buttons
+//on screen
+
+//step 2: make changes to button based on event.
+
+function pianoPress(){
+    let Key = document.getElementById("c key");
+
+    Key.addEventListener("keydown", (event) => {
+         console.log(`${event.key}`);
+    });
+  console.log(Key);
+
+}
+pianoPress();
